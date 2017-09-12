d41d5adcd5fc92868d22f116b2aa9aa2a6799e47
T.J. Harris <tyrone.harrisjr0118@gmail.com>
2017-09-12 16:34:27 -0400

    added contact info page added payment page

--- src/main/java/com/amc/ContactInfoPage.java
+++ src/main/java/com/amc/ContactInfoPage.java
@@ -3,11 +3,16 @@ package src.main.java.com.amc;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ContactInfoPage {
 
     private AndroidDriver<MobileElement> driver;
+
+    //Misc elements
+    @FindBy(xpath="//android.widget.TextView[@text='Contact Info']") public WebElement page_header;
 
     public ContactInfoPage(AndroidDriver<MobileElement> driver) {
         this.driver = driver;
--- src/main/java/com/amc/PaymentPage.java
+++ src/main/java/com/amc/PaymentPage.java
@@ -3,11 +3,16 @@ package src.main.java.com.amc;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PaymentPage {
 
     private AndroidDriver<MobileElement> driver;
+
+    //Misc elements
+    @FindBy(xpath="//android.widget.TextView[@text='Payment']") public WebElement page_header;
 
     public PaymentPage(AndroidDriver<MobileElement> driver) {
         this.driver = driver;
